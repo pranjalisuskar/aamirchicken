2bfb07fd18f21564a18689ff8a218a79872a15f1
= <=>
2024-11-27 22:35:15 +0530

    :::ProductTable Working:::

--- src/Component/componentadmin/Productcomponent/Producttable.js
+++ src/Component/componentadmin/Productcomponent/Producttable.js
@@ -11,7 +11,6 @@ import {
   Row,
 } from "reactstrap";
 
-
 const Producttable = () => {
   const [shopData, setShopData] = useState([]);
   const [addModal, setAddModal] = useState(false);
@@ -40,68 +39,274 @@ const Producttable = () => {
   }, []);
 
   return (
-    <div className="page-content">
-      <div className="fluid " style={{marginTop:'20px'}}>
-        < div className="row">
-          <div className="col-lg-12" >
-            {/* <Card className="shadow-sm"> */}
-            <div className="shadow-sm">
-              <CardHeader className="bg-danger text-white rounded">
-                <Row className="gy-3 align-items-center">
-                  <Col xs={12} sm={6}>
-                    <h5 className="mb-0">🛍 Product List</h5>
-                  </Col>
-                  <Col xs={12} sm={6} className="text-end">
-                    <button
-                      className="btn btn-light btn-sm text-danger"
-                      onClick={toggleAddModal}
-                      style={{marginRight:'80px'}}
-                    >
-                      <i className="ri-add-line"></i> Add Product
-                    </button>
-                  </Col>
-                </Row>
-              </CardHeader>
-              {/* <CardBody> */}
-              <div>
-                <div className="table-responsive">
-                  <table className="table align-middle table-hover table-striped">
-                    <thead>
-                      <tr>
-                        <th>Sr No</th>
-                        <th>product Name</th>
-                        <th>Action</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-  <tr>
-    <td>1</td>
-    <td>hjk</td>
-    <td className="d-flex gap-2">
-      <button className="btn btn-sm btn-outline-primary">
-        View
-      </button>
-      <button className="btn btn-sm btn-outline-secondary">
-        Edit
-      </button>
-      <button className="btn btn-sm btn-outline-danger">
-        Delete
-      </button>
-    </td>
-  </tr>
-  <tr>
-    <td colSpan="3" className="text-center">
-      No shops available.
-    </td>
-  </tr>
-</tbody>
+    <div
+      className=""
+      style={{ width: "1350px", marginLeft: "80px", marginBottom: "80px" }}
+    >
+      <div className="row" style={{ marginTop: "80px" }}>
+        <div className="col-lg-12">
+          <div className="shadow-sm">
+            <CardHeader className="bg-danger text-white rounded">
+              <Row
+                className="gy-3 align-items-center"
+                style={{ backgroundColor: "#9a292f" }}
+              >
+                <Col>
+                  <h5
+                    className="mb-0"
+                    style={{
+                      color: "white",
+                      fontWeight: "bold",
+                      marginBottom: "5px",
+                      marginLeft: "5px",
+                    }}
+                  >
+                    Product List
+                  </h5>
+                </Col>
+                <Col xs={12} sm={6} className="text-end">
+                  <button
+                    className="btn btn-light btn-sm "
+                    onClick={toggleAddModal}
+                    style={{
+                      marginBottom: "10px",
+                      width: "150px",
+                      marginRight: "5px",
+                      color: "#9a292f",
+                    }}
+                  >
+                    <i className="ri-add-line" style={{ color: "#9a292f" }}></i>{" "}
+                    Add Product
+                  </button>
+                </Col>
+              </Row>
+            </CardHeader>
+            <div>
+              <div className="table-responsive">
+                <table
+                  className="table align-middle table-hover table-striped"
+                  style={{ width: "1350px" }}
+                >
+                  <thead>
+                    <tr>
+                      <th>Sr No</th>
+                      <th>Product Name</th>
+                      <th>Product Price</th>
+                      <th>Product Category</th>
+                      <th>Product Image</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Chiken</td>
+                      <td>200</td>
+                      <td>abc</td>
+                      <td>
+                        <img
+                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-ReGjnihI1IwgIL8_KJRWOff8lZWFlJb7xg&s"
+                          alt="Image description"
+                          style={{
+                            width: "50px",
+                            height: "50px",
+                            objectFit: "cover",
+                          }}
+                        />
+                      </td>
 
-                  </table>
-                </div>
-                </div>
-                </div>
-              {/* </CardBody> */}
-            {/* </Card> */}
+                      <td className="d-flex gap-2" style={{ border: "none" }}>
+                        <span>
+                          <i
+                            className="ri-eye-line"
+                            style={{
+                              fontSize: "1.2rem",
+                              textDecoration: "none",
+                              border: "none",
+                            }}
+                          ></i>
+                        </span>
+                        <span>
+                          <i
+                            className="ri-edit-line"
+                            style={{
+                              fontSize: "1.2rem",
+                              color: "green",
+                              textDecoration: "none",
+                              border: "none",
+                            }}
+                          ></i>
+                        </span>
+                        <span>
+                          <i
+                            className="ri-delete-bin-line"
+                            style={{
+                              fontSize: "1.2rem",
+                              color: "red",
+                              textDecoration: "none",
+                              border: "none",
+                            }}
+                          ></i>
+                        </span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>Mutton</td>
+                      <td>100</td>
+                      <td>abc</td>
+                      <td>
+                        <img
+                          src="https://i0.wp.com/legpiece.in/wp-content/uploads/2021/05/Mutton-Curry-Cut.jpg?fit=640%2C640&ssl=1"
+                          alt="Image description"
+                          style={{
+                            width: "50px",
+                            height: "50px",
+                            objectFit: "cover",
+                          }}
+                        />
+                      </td>
+                      <td className="d-flex gap-2" style={{ border: "none" }}>
+                        <span>
+                          <i
+                            className="ri-eye-line"
+                            style={{
+                              fontSize: "1.2rem",
+                              textDecoration: "none",
+                              border: "none",
+                            }}
+                          ></i>
+                        </span>
+                        <span>
+                          <i
+                            className="ri-edit-line"
+                            style={{
+                              fontSize: "1.2rem",
+                              color: "green",
+                              textDecoration: "none",
+                              border: "none",
+                            }}
+                          ></i>
+                        </span>
+                        <span>
+                          <i
+                            className="ri-delete-bin-line"
+                            style={{
+                              fontSize: "1.2rem",
+                              color: "red",
+                              textDecoration: "none",
+                              border: "none",
+                            }}
+                          ></i>
+                        </span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>Fish</td>
+                      <td>100</td>
+                      <td>abc</td>
+                      <td>
+                        <img
+                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDVW2TRmrDC5HRAmDcluJZMvUitjSzIiXJ9A&s"
+                          alt="Image description"
+                          style={{
+                            width: "50px",
+                            height: "50px",
+                            objectFit: "cover",
+                          }}
+                        />
+                      </td>
+                      <td className="d-flex gap-2" style={{ border: "none" }}>
+                        <span>
+                          <i
+                            className="ri-eye-line"
+                            style={{
+                              fontSize: "1.2rem",
+                              textDecoration: "none",
+                              border: "none",
+                            }}
+                          ></i>
+                        </span>
+                        <span>
+                          <i
+                            className="ri-edit-line"
+                            style={{
+                              fontSize: "1.2rem",
+                              color: "green",
+                              textDecoration: "none",
+                              border: "none",
+                            }}
+                          ></i>
+                        </span>
+                        <span>
+                          <i
+                            className="ri-delete-bin-line"
+                            style={{
+                              fontSize: "1.2rem",
+                              color: "red",
+                              textDecoration: "none",
+                              border: "none",
+                            }}
+                          ></i>
+                        </span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>4</td>
+                      <td>Eggs</td>
+                      <td>100</td>
+                      <td>abc</td>
+                      <td>
+                        <img
+                          src="https://media.post.rvohealth.io/wp-content/uploads/2020/09/health-benefits-of-eggs-732x549-thumbnail-732x549.jpg"
+                          alt="Image description"
+                          style={{
+                            width: "50px",
+                            height: "50px",
+                            objectFit: "cover",
+                          }}
+                        />
+                      </td>
+                      <td className="d-flex gap-2" style={{ border: "none" }}>
+                        <span>
+                          <i
+                            className="ri-eye-line"
+                            style={{
+                              fontSize: "1.2rem",
+                              textDecoration: "none",
+                              border: "none",
+                            }}
+                          ></i>
+                        </span>
+                        <span>
+                          <i
+                            className="ri-edit-line"
+                            style={{
+                              fontSize: "1.2rem",
+                              color: "green",
+                              textDecoration: "none",
+                              border: "none",
+                            }}
+                          ></i>
+                        </span>
+                        <span>
+                          <i
+                            className="ri-delete-bin-line"
+                            style={{
+                              fontSize: "1.2rem",
+                              color: "red",
+                              textDecoration: "none",
+                              border: "none",
+                            }}
+                          ></i>
+                        </span>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -109,92 +314,90 @@ const Producttable = () => {
       <Modal isOpen={addModal} toggle={toggleAddModal} size="lg">
         <ModalHeader toggle={toggleAddModal}>Add product</ModalHeader>
         <ModalBody>
-        <form
-        className="shopform"
-        encType="multipart/form-data"
-        // onSubmit={onSubmit}
-      >
-        <div className="row">
-          <div className="form-group col-md-6 custom-input">
-            <input
-              className="form-control triangle-input"
-              type="text"
-              name="productName"
-              placeholder="Product Name"
-            //   value={productData.productName}
-            //   onChange={onInputChange}
-            />
-          </div>
-          <div className="form-group col-md-6 custom-input">
-            <input
-              className="form-control triangle-input"
-              type="text"
-              name="price"
-              placeholder="Product Price"
-            //   value={productData.price}
-            //   onChange={onInputChange}
-            />
-          </div>
-          <div className="form-group col-md-6 custom-input">
-            <input
-              className="form-control triangle-input"
-              type="text"
-              name="category"
-              placeholder="Product Category"
-            //   value={productData.category}
-            //   onChange={onInputChange}
-            />
-          </div>
-          <div className="form-group col-md-6 custom-input">
-            <button
-              type="button"
-            //   onClick={() => document.getElementById("shopImages").click()}
-              style={{
-                display: "inline-block",
-                padding: "10px 20px",
-                backgroundColor: "#7a2226",
-                color: "#fff",
-                borderRadius: "5px",
-                cursor: "pointer",
-                textAlign: "center",
-                border: "none",
-              }}
-            >
-              Choose Images
-            </button>
-            <input
-              type="file"
-              id="shopImages"
-              name="image_url"
-              multiple
-              className="form-control-file"
-            //   onChange={(e) => handleImageChange(e)}
-              style={{ display: "none"}}
-            />
-            <div className="image-preview-container mt-3">
-              
-                <img
-                 
-                  src=''
-                  alt=''
-                  className="image-preview"
-                  style={{
-                    width: "100px",
-                    height: "100px",
-                    objectFit: "cover",
-                    margin: "5px",
-                    borderRadius: "5px",
-                    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-                  }}
+          <form
+            className="shopform"
+            encType="multipart/form-data"
+            // onSubmit={onSubmit}
+          >
+            <div className="row">
+              <div className="form-group col-md-6 custom-input">
+                <input
+                  className="form-control triangle-input"
+                  type="text"
+                  name="productName"
+                  placeholder="Product Name"
+                  //   value={productData.productName}
+                  //   onChange={onInputChange}
                 />
-              {/* ))} */}
+              </div>
+              <div className="form-group col-md-6 custom-input">
+                <input
+                  className="form-control triangle-input"
+                  type="text"
+                  name="price"
+                  placeholder="Product Price"
+                  //   value={productData.price}
+                  //   onChange={onInputChange}
+                />
+              </div>
+              <div className="form-group col-md-6 custom-input">
+                <input
+                  className="form-control triangle-input"
+                  type="text"
+                  name="category"
+                  placeholder="Product Category"
+                  //   value={productData.category}
+                  //   onChange={onInputChange}
+                />
+              </div>
+              <div className="form-group col-md-6 custom-input">
+                <button
+                  type="button"
+                  //   onClick={() => document.getElementById("shopImages").click()}
+                  style={{
+                    display: "inline-block",
+                    padding: "10px 20px",
+                    backgroundColor: "#7a2226",
+                    color: "#fff",
+                    borderRadius: "5px",
+                    cursor: "pointer",
+                    textAlign: "center",
+                    border: "none",
+                  }}
+                >
+                  Choose Images
+                </button>
+                <input
+                  type="file"
+                  id="shopImages"
+                  name="image_url"
+                  multiple
+                  className="form-control-file"
+                  //   onChange={(e) => handleImageChange(e)}
+                  style={{ display: "none" }}
+                />
+                <div className="image-preview-container mt-3">
+                  <img
+                    src=""
+                    alt=""
+                    className="image-preview"
+                    style={{
+                      width: "100px",
+                      height: "100px",
+                      objectFit: "cover",
+                      margin: "5px",
+                      borderRadius: "5px",
+                      boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                    }}
+                  />
+                  {/* ))} */}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <button type="submit" className="submit-button">
-          Submit
-        </button>
-      </form>
+            <button type="submit" className="submit-button">
+              Submit
+            </button>
+          </form>
         </ModalBody>
       </Modal>
 
